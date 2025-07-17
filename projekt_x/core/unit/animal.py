d5d652f225199aa9006061d2f06f9761e9439309
pickelmann <alex.doepfer@gmail.com>
2025-07-17 05:37:09 +0200

    Delete projekt_x directory

--- projekt_x/core/unit/animal.py
+++
@@ -1,8 +0,0 @@
-from core.unit.base import Unit
-
-class Animal(Unit):
-    def __init__(self, name, x, y, z):
-        super().__init__(name, x, y, z, traits=["needs_oxygen", "animal"])
-
-    def get_entity_type(self):
-        return "animal"
--- projekt_x/core/unit/robot.py
+++
@@ -1,8 +0,0 @@
-from core.unit.base import Unit
-
-class robot(Unit):
-    def __init__(self, name, x, y, z):
-        super().__init__(name, x, y, z, traits=["can_dig"])
-
-    def get_entity_type(self):
-        return "robot"
